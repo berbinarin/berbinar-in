3f079525e5c2e3a7c91738995b983029899f602e
gustonecrush <codeboxezz@gmail.com>
2023-08-22 20:43:42 +0700

    edited work with us design text to berbar.in

--- resources/views/work-with-us.blade.php
+++ resources/views/work-with-us.blade.php
@@ -7,9 +7,9 @@
 @section('content')
 <section class="flex bg-white w-full h-screen relative items-center justify-center">
     <div class="flex items-center justify-center flex-col -mt-14">
-        <img src="https://i.postimg.cc/L5BLFJFL/by-dhila-banner-web-2.png" alt="Work With Us" title="Work With Us" class="w-[35rem]" />
+        <img src="https://i.postimg.cc/PJvNB79m/by-dhila-banner-web.png" alt="Work With Us" title="Work With Us" class="w-[35rem]" />
 
-        <h1 class="home__title md:w-full w-[25rem] md:text-[2rem] text-center leading-[120%] mt-10">Ayo Kerja Sama Bersama <br>Berbinar Insightful Indonesia</h1>
+        <h1 class="home__title md:w-full w-[25rem] md:text-[2rem] text-center leading-[120%]">Ayo Kerja Sama Bersama <br>Berbinar Insightful Indonesia</h1>
         <p class="home__description text-center w-[50%]">Berbinar hadir menawarkan layanan konseling bersama psikolog partner dan peer counselor untuk setiap individu yang sedang berjuang.</p>
         <a href="https://bit.ly/CounselingWithBerbinar" target="_blank" class="button">Hubungi Kami</a>
     </div>
